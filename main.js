50857a380fe5c8f5f933c08343b8ed9facb3fccf
Jacob Dowdle <jacobk.dowdle@gmail.com>
2022-09-06 18:43:38 +0800

    trying to solve cacheDOM issue

--- main.js
+++ main.js
@@ -6,27 +6,37 @@
         mode: 'human',
         init: function() {
             this.cacheDom();
+            this.bindEvents();
+            console.log(this.modeBtns)
         },
         cacheDom: function() {
-            this.el = document.getElementById('gameZone');
+            this.el = document.getElementById('contentContainer');
             this.annoucements = this.el.querySelector('#announcements');
+            this.modeBtns = this.el.querySelectorAll('.modeBtn');
             this.symbolBtns = this.el.querySelectorAll('.symbolBtn');
             this.gameSquares = this.el.querySelectorAll('.gameSquare');
             this.resetBtn = this.el.querySelector('#resetBtn');
+            this.opponent = this.el.querySelector('#oppenentSelected');
         },
         bindEvents: function() {
-
+            for (let i=0; i < this.modeBtns.length; i++) {
+                this.modeBtns[i].addEventListener('click', this.modeSelect);
+            }
         },
         populateGameBoard: function() {
             for (let i=0; i<9; i++) {
                 this.gameSquares[i].innerHTML = this.gameBoard[i];
-            }
+            };
         },
         userPick: function() {
 
         },
         symbolSelect: function() {
 
+        },
+        modeSelect: function() {
+            this.mode = this.value;
+            console.log(this.el)
         }
     }
     ticTacToe.init();
